d1beaa04b79e9e9549ce447e26b3c9a07ee9cc5b
Predater7 <3157972067@qq.com>
2024-12-18 10:13:35 +0800

    music
    music

--- TodayIsAGoodDay/proj.win32/Setting/setting.h
+++ TodayIsAGoodDay/proj.win32/Setting/setting.h
@@ -1,2 +1,7 @@
 #pragma once
 int get_window_size();
+int preload_BGM();
+void play_BGM(bool flag);
+void volume_adjustment(float num);
+MenuItemToggle* createVolumeButton();
+ui::Slider* createVolumeSlider();
