91df1c621b8a2d2666cffe390844f877a0908338
HaoSheng Dai <a44949522@126.com>
2024-12-20 01:51:14 +0800

    12/20 2p.m.

--- TodayIsAGoodDay/proj.win32/Person/action.cpp
+++ TodayIsAGoodDay/proj.win32/Person/action.cpp
@@ -3,5 +3,15 @@
 
 void Person::useTools()
 {
+	currentTool->_toolsprite = Sprite::create("/tool/" + currentTool->getName() + "1.png");
+	auto currentScene = Director::getInstance()->getRunningScene();
+	currentScene->addChild(currentTool->_toolsprite);
+	currentTool->_toolsprite->setPosition(getWorldPosition() + Vec2(64, 48));
 
+	//模拟动画效果
+	auto rotateCounterClockwise = RotateBy::create(0.5f, -45.0f); // 逆时针旋转 45 度，耗时 0.5 秒
+	auto pause = DelayTime::create(0.1f); // 暂停 0.1 秒
+	auto rotateClockwise = RotateBy::create(0.5f, 90.0f); // 顺时针旋转 90 度，耗时 0.5 秒
+	auto useSequence = Sequence::create(rotateCounterClockwise, pause, rotateClockwise, nullptr);
+	currentTool->_toolsprite->runAction(useSequence);
 }
